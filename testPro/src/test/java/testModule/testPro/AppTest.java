98ad32d67835a922f839648205dea058f5a358ef
Chandu <chandu.alahari@gmail.com>
2018-11-02 10:00:47 -0700

    4th checkin

--- testPro/src/test/java/testModule/testPro/AppTest.java
+++ testPro/src/test/java/testModule/testPro/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite15()
+    public static Test suite16()
     {
         return new TestSuite( AppTest.class );
     }
