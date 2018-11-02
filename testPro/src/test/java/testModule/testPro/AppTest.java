62b13f43aebe1a8c3eb27b7c8772ca955b36f6af
Chandu <chandu.alahari@gmail.com>
2018-11-02 10:05:24 -0700

    merge test

--- testPro/src/test/java/testModule/testPro/AppTest.java
+++ testPro/src/test/java/testModule/testPro/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite17()
+    public static Test suite21()
     {
         return new TestSuite( AppTest.class );
     }
